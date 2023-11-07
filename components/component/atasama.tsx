13a44e06719782b0cdcb260f04fcd56e6b8afd02
Daniel Barcellos <dbarcelloz@gmail.com>
2023-11-07 13:14:04 -0300

    typo lol

--- components/component/atasama.tsx
+++ components/component/atasama.tsx
@@ -7,7 +7,7 @@ import { Button } from "@/components/ui/button"
 export function Atasama() {
   return (
     <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex flex-col items-center justify-center">
-      <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-10">ASATAMA</h1>
+      <h1 className="text-4xl font-bold text-gray-800 dark:text-white mb-10">ATASAMA</h1>
       <div className="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-6 w-full max-w-md">
         <h2 className="text-2xl font-semibold text-gray-700 dark:text-gray-200 mb-4">ROUTINE</h2>
         <div className="border-b border-gray-200 dark:border-gray-600 py-4">
